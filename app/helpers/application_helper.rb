b2a685bfb876011568deefd7416e79f57a386ac7
Mong-Kuen Sun <mongkuen.sun@gmail.com>
2015-02-05 16:40:18 -0600

    Added forms/checkboxes, categories, comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fix_url(str)
+    str.starts_with?("http://") ? str : "http://#{str}"
+  end
+
+  def display_datetime(time)
+    time.strftime("%m/%d/%Y %l:%M%P %Z") #03/14/2013 9:09pm
+  end
 end
